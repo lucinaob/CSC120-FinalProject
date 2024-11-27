92ec199d19d5773f191e345d99644debcddcc669
ashmessier <ashleymess122@gmail.com>
2024-11-27 18:20:24 -0500

    added being features

--- Being.java
+++ Being.java
@@ -1,9 +1,22 @@
 public class Being {
     String name; 
-    celestialBody location; // why wont you do this 
+    boolean alive;
+    celestialBody location; // why it sync correctly? 
 
     public Being(String name, celestialBody location){
         this.name = name;
+        this.alive = true; // being starts out alive 
         this.location = location;
     }
+
+    // updates location 
+    public void travel(celestialBody location){
+        this.location = location; 
+    }
+
+    // sets creature's alive status to false  
+    public void die(){
+        this.alive = false;  // sets alive to false - dead
+    }
+
 }
